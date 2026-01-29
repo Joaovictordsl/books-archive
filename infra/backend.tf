b2be95c8f9c43d2647d897a36866e13deb26291f
Joao Victor de Sousa Lima <joaovictordsl00@gmail.com>
2026-01-29 18:29:20 -0300

    iam

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,16 +1,16 @@
-#terraform {
-# backend "s3"{
-
-#  bucket = "my-s3-bucket-johnny-books-archive"
-#  region = "us-east-1"
-#  key = "books-archive/terraform.tfsate"
-#  encrypt = true
-#
-#  }
-#}
-
-resource "aws_s3_bucket" "bucket" {
+terraform {
+ backend "s3"{
 
   bucket = "my-s3-bucket-johnny-books-archive"
-
+  region = "us-east-1"
+  key = "books-archive/terraform.tfsate"
+  encrypt = true
+  use_lockfile = true 
+  }
 }
+
+#resource "aws_s3_bucket" "bucket" {
+
+#  bucket = "my-s3-bucket-johnny-books-archive"
+
+#}
---
+++ infra/ec2.tf
@@ -0,0 +1,58 @@
+resource "tls_private_key" "rsa_key"{
+  algorithm = "RSA"
+  rsa_bits = 4096
+}
+
+resource "aws_key_pair" "key_pair" {
+  key_name = "my-key-terraform"
+  public_ key = tls_private_key.rsa_key.public_key_openssh
+}
+
+resource "local_file" "private_key"{
+  content = tls_private_key.rsa_key.private_key_pem
+  filename = "my-key-terraform.pem"
+}
+
+####
+
+resource "aws_security_group" "security_group" {
+  name = "security_group"
+  description = "Allow HTTP/HTTPS and SSH connections"
+
+  #HTTP
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+  ingress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["191.185.181.57/32"]
+  }
+
+  engress {
+    from_port = 0
+    to_port = 65350
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
+resource "aws_instance" "ec2-server" {
+  ami = "ami-0532be01f26a3de55"
+  instance_type = "t3.micro"
+
+}
